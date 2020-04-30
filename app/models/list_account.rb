dbe5461df202a2fd341bec48b05767e3c8ecde3f
mgabdev <>
2020-04-30 12:34:13 -0400

    Non-follows allowed in lists

--- app/models/list_account.rb
+++ app/models/list_account.rb
@@ -12,7 +12,7 @@
 class ListAccount < ApplicationRecord
   belongs_to :list
   belongs_to :account
-  belongs_to :follow
+  belongs_to :follow, optional: true
 
   validates :account_id, uniqueness: { scope: :list_id }
 
@@ -21,6 +21,6 @@ class ListAccount < ApplicationRecord
   private
 
   def set_follow
-    self.follow = Follow.find_by(account_id: list.account_id, target_account_id: account.id)
+    self.follow = Follow.find_by!(account_id: list.account_id, target_account_id: account.id) unless list.account_id == account.id
   end
 end
