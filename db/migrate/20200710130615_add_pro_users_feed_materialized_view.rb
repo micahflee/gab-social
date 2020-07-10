d5293c2e1d64e9e29376f2f71608b92c50787779
admin1 <admin@gab.com>
2020-07-10 09:33:13 -0400

    added pro_users_feed_matview

---
+++ db/migrate/20200710130615_add_pro_users_feed_materialized_view.rb
@@ -0,0 +1,13 @@
+class AddProUsersFeedMaterializedView < ActiveRecord::Migration[5.2]
+  def change
+    execute <<-SQL
+      CREATE MATERIALIZED VIEW pro_feed_users_matview AS
+        SELECT id 
+        FROM accounts
+        WHERE accounts.is_investor=true 
+          OR accounts.is_donor=true 
+          OR accounts.is_verified=true 
+          OR accounts.is_pro=true
+    SQL
+  end
+end
