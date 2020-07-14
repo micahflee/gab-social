81433b49fc09a63033210d55e806532719d58665
mgabdev <>
2020-07-14 01:01:36 -0500

    Removed Introduction code from gabsocial.js
    • Removed:
    - Introduction code from gabsocial.js

--- app/javascript/gabsocial/containers/gabsocial.js
+++ app/javascript/gabsocial/containers/gabsocial.js
@@ -2,7 +2,6 @@
 
 import { Provider } from 'react-redux'
 import configureStore from '../store/configureStore'
-import { INTRODUCTION_VERSION } from '../actions/onboarding'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { ScrollContext } from 'react-router-scroll-4'
 import { IntlProvider, addLocaleData } from 'react-intl'
@@ -16,7 +15,6 @@ import { getLocale } from '../locales'
 import initialState from '../initial_state'
 import { me } from '../initial_state'
 import UI from '../features/ui'
-// import Introduction from '../features/introduction'
 import ErrorBoundary from '../components/error_boundary'
 import Display from './display'
 
@@ -29,30 +27,9 @@ const hydrateAction = hydrateStore(initialState)
 store.dispatch(hydrateAction)
 store.dispatch(fetchCustomEmojis())
 
-const mapStateToProps = (state) => {
-  const account = state.getIn(['accounts', me])
-  const showIntroduction = account ? state.getIn(['settings', 'introductionVersion'], 0) < INTRODUCTION_VERSION : false
-
-  return {
-    showIntroduction,
-  }
-}
-
-@connect(mapStateToProps)
 class GabSocialMount extends PureComponent {
 
-  static propTypes = {
-    showIntroduction: PropTypes.bool,
-  }
-
   render () {
-    // Disabling introduction for launch
-    // const { showIntroduction } = this.props
-    //
-    // if (showIntroduction) {
-    //   return <Introduction />
-    // }
-
     return (
       <BrowserRouter>
         <ScrollContext>
