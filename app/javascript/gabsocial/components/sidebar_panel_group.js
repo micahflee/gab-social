81ddccbdfd597c6065724e6c78a6b192d33def8e
mgabdev <>
2020-07-15 23:00:44 -0500

    Added SidebarPanelGroup for StatusPromotionPanel
    • Added:
    - SidebarPanelGroup for StatusPromotionPanel

---
+++ app/javascript/gabsocial/components/sidebar_panel_group.js
@@ -0,0 +1,33 @@
+import { Fragment } from 'react'
+import { me, promotions } from '../initial_state'
+import StatusPromotionPanel from './panel/status_promotion_panel'
+
+export default class SidebarPanelGroup extends PureComponent {
+
+  static propTypes = {
+    layout: PropTypes.array.isRequired,
+    page: PropTypes.string.isRequired,
+    promotion: PropTypes.object,
+  }
+
+  render() {
+    const { layout, page } = this.props
+
+    if (Array.isArray(promotions) && Array.isArray(layout) && !!me) {
+      const sidebarPromotionPageId = `${page}.sidebar`
+      const promotion = promotions.find(p => p.timeline_id === sidebarPromotionPageId)
+
+      if (!!promotion) {
+        const correctedPosition = promotion.position - 1 > layout.length ? layout.length - 1 : promotion.position
+        layout.splice(correctedPosition, 0, <StatusPromotionPanel key='status-promotion-panel' statusId={promotion.status_id} />)
+      }
+    }
+
+    return (
+      <Fragment>
+        {layout}
+      </Fragment>
+    )
+  }
+
+}
