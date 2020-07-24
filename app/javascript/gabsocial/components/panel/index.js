6b3b488234429ad5f173aa9f9b381d3797246c36
mgabdev <>
2020-07-24 18:51:25 -0500

    Removed unused index.js in panel folder
    • Removed:
    - unused index.js in panel folder

--- app/javascript/gabsocial/components/panel/index.js
+++
@@ -1,9 +0,0 @@
-import SignUpPanel from './sign_up_panel';
-import TrendsPanel from './trends_panel';
-import WhoToFollowPanel from './who_to_follow_panel';
-
-export {
-  SignUpPanel,
-  TrendsPanel,
-  WhoToFollowPanel,
-}
