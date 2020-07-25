853b15a63e043b15ab53c464d584594bd980269c
mgabdev <>
2020-07-24 21:20:09 -0500

    Updated UnauthorizedModal styles
    • Updated:
    - UnauthorizedModal styles

--- app/javascript/gabsocial/components/modal/unauthorized_modal.js
+++ app/javascript/gabsocial/components/modal/unauthorized_modal.js
@@ -5,11 +5,9 @@ import Text from '../text'
 import Button from '../button'
 
 const messages = defineMessages({
-  close: { id: 'lightbox.close', defaultMessage: 'Close' },
   signup: { id: 'unauthorized_modal.title', defaultMessage: 'Sign up for Gab' },
   text: { id: 'unauthorized_modal.text', defaultMessage: 'You need to be logged in to do that.' },
   register: { id: 'account.register', defaultMessage: 'Sign up' },
-  alreadyHaveAccount: { id: 'unauthorized_modal.footer', defaultMessage: 'Already have an account? {login}.' },
   login: { id: 'account.login', defaultMessage: 'Log in' },
 })
 
@@ -27,32 +25,39 @@ class UnauthorizedModal extends ImmutablePureComponent {
 
     return (
       <ModalLayout
+        hideClose
         title={intl.formatMessage(messages.signup)}
         onClose={onClose}
-        width={400}
+        width={480}
       >
-        <div className={[_s.default, _s.px10, _s.py10].join(' ')}>
-          <Text className={_s.mb15} align='center'>
-            {intl.formatMessage(messages.text)}
-          </Text>
-          <Button isBlock href='/auth/sign_up' className={[_s.mr15, _s.ml15, _s.mlAuto, _s.mrAuto].join(' ')}>
-            {intl.formatMessage(messages.register)}
-          </Button>
-        </div>
-        <div className={[_s.default, _s.px10, _s.py5].join(' ')}>
-          <Text color='secondary' size='small' align='center'>
-            {
-              intl.formatMessage(messages.login, {
-                login: (
-                  <Button text href='/auth/sign_in'>
-                    {intl.formatMessage(messages.login)}
-                  </Button>
-                )
-              })
-            }
-          </Text>
+        <div className={[_s.default, _s.mt15, _s.mb15, _s.px15, _s.pb15].join(' ')}>
+          <div className={[_s.default, _s.px15, _s.py15].join(' ')}>
+            <Text className={[_s.px15, _s.py15, _s.mb15]} size='medium' weight='medium' align='center'>
+              {intl.formatMessage(messages.text)}
+            </Text>
+
+            <Button isBlock href='/auth/sign_up' className={[_s.mt15, _s.mlAuto, _s.mrAuto].join(' ')}>
+              <Text color='inherit' size='large' weight='bold' align='center'>
+                {intl.formatMessage(messages.register)}
+              </Text>
+            </Button>
+
+            <Button
+              isOutline
+              isBlock
+              href='/auth/sign_in'
+              color='brand'
+              backgroundColor='none'
+              className={[_s.mt15, _s.mlAuto, _s.mrAuto].join(' ')}
+            >
+              <Text color='inherit' size='large' weight='bold' align='center'>
+                {intl.formatMessage(messages.login)}
+              </Text>
+            </Button>
+          </div>
         </div>
       </ModalLayout>
     )
   }
+
 }
