19b86ccc8b56ff792b7906cc98ae8e304375a237
mgabdev <>
2020-08-05 23:58:59 -0500

    Removed unused code from GroupTimeline feature
    • Removed:
    - unused code from GroupTimeline feature

--- app/javascript/gabsocial/features/group_timeline.js
+++ app/javascript/gabsocial/features/group_timeline.js
@@ -1,8 +1,6 @@
 import ImmutablePureComponent from 'react-immutable-pure-component'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import { injectIntl, defineMessages } from 'react-intl'
-import { Link } from 'react-router-dom'
-import classNames from 'classnames'
 import { me } from '../initial_state'
 import { connectGroupStream } from '../actions/streaming'
 import { expandGroupTimeline } from '../actions/timelines'
@@ -10,16 +8,11 @@ import StatusList from '../components/status_list'
 import ColumnIndicator from '../components/column_indicator'
 
 const messages = defineMessages({
-	tabLatest: { id: 'group.timeline.tab_latest', defaultMessage: 'Latest' },
-	show: { id: 'group.timeline.show_settings', defaultMessage: 'Show settings' },
-	hide: { id: 'group.timeline.hide_settings', defaultMessage: 'Hide settings' },
 	empty: { id: 'empty_column.group', defaultMessage: 'There is nothing in this group yet.\nWhen members of this group post new statuses, they will appear here.' },
 })
 
 const mapStateToProps = (state, props) => ({
 	group: state.getIn(['groups', props.params.id]),
-	relationships: state.getIn(['group_relationships', props.params.id]),
-	hasUnread: state.getIn(['timelines', `group:${props.params.id}`, 'unread']) > 0,
 })
 
 export default
@@ -27,24 +20,14 @@ export default
 @injectIntl
 class GroupTimeline extends ImmutablePureComponent {
 
-	static contextTypes = {
-		router: PropTypes.object,
-	}
-
 	static propTypes = {
 		params: PropTypes.object.isRequired,
 		dispatch: PropTypes.func.isRequired,
-		hasUnread: PropTypes.bool,
 		group: PropTypes.oneOfType([
 			ImmutablePropTypes.map,
 			PropTypes.bool,
 		]),
-		relationships: ImmutablePropTypes.map,
 		intl: PropTypes.object.isRequired,
-	}
-
-	state = {
-		collapsed: true,
 	}
 
 	componentDidMount() {
@@ -70,14 +53,8 @@ class GroupTimeline extends ImmutablePureComponent {
 		this.props.dispatch(expandGroupTimeline(id, { maxId }))
 	}
 
-	handleToggleClick = (e) => {
-		e.stopPropagation()
-		this.setState({ collapsed: !this.state.collapsed })
-	}
-
 	render() {
-		const { group, relationships, account, intl } = this.props
-		const { collapsed } = this.state
+		const { group, intl } = this.props
 		const { id } = this.props.params
 
 		if (typeof group === 'undefined') {
@@ -88,11 +65,9 @@ class GroupTimeline extends ImmutablePureComponent {
 
 		return (
 			<StatusList
-				alwaysPrepend
-				scrollKey={`group_timeline-${id}`}
+				scrollKey={`group-timeline-${id}`}
 				timelineId={`group:${id}`}
 				onLoadMore={this.handleLoadMore}
-				group={group}
 				emptyMessage={intl.formatMessage(messages.empty)}
 			/>
 		)
