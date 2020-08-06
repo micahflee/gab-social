c0f7e3adbf831ed3f233949dc5b6e8bb45c7dc27
mgabdev <>
2020-08-05 23:21:35 -0500

    Removed semicolons from LoadingBar middleware
    • Removed:
    - semicolons from LoadingBar middleware

--- app/javascript/gabsocial/middleware/loading_bar.js
+++ app/javascript/gabsocial/middleware/loading_bar.js
@@ -1,25 +1,25 @@
-import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
-const defaultTypeSuffixes = ['PENDING', 'FULFILLED', 'REJECTED'];
+const defaultTypeSuffixes = ['PENDING', 'FULFILLED', 'REJECTED']
 
 export default function loadingBarMiddleware(config = {}) {
-  const promiseTypeSuffixes = config.promiseTypeSuffixes || defaultTypeSuffixes;
+  const promiseTypeSuffixes = config.promiseTypeSuffixes || defaultTypeSuffixes
 
   return ({ dispatch }) => next => (action) => {
     if (action.type && !action.skipLoading) {
-      const [PENDING, FULFILLED, REJECTED] = promiseTypeSuffixes;
+      const [PENDING, FULFILLED, REJECTED] = promiseTypeSuffixes
 
-      const isPending = new RegExp(`${PENDING}$`, 'g');
-      const isFulfilled = new RegExp(`${FULFILLED}$`, 'g');
-      const isRejected = new RegExp(`${REJECTED}$`, 'g');
+      const isPending = new RegExp(`${PENDING}$`, 'g')
+      const isFulfilled = new RegExp(`${FULFILLED}$`, 'g')
+      const isRejected = new RegExp(`${REJECTED}$`, 'g')
 
       if (action.type.match(isPending)) {
-        dispatch(showLoading());
+        dispatch(showLoading())
       } else if (action.type.match(isFulfilled) || action.type.match(isRejected)) {
-        dispatch(hideLoading());
+        dispatch(hideLoading())
       }
     }
 
-    return next(action);
-  };
-};
+    return next(action)
+  }
+}
