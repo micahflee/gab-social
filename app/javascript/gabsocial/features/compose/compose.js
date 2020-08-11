79d77ec0888c51b714370d1f1f682e2c6f67a349
mgabdev <>
2020-08-10 22:01:09 -0500

    Fixed issue with quote/reply not canceling on mobile page pop
    • Fixed:
    - issue with quote/reply not canceling on mobile page pop

--- app/javascript/gabsocial/features/compose/compose.js
+++ app/javascript/gabsocial/features/compose/compose.js
@@ -1,6 +1,21 @@
+import { clearCompose } from '../../actions/compose'
 import ComposeFormContainer from './containers/compose_form_container'
 
-export default class Compose extends PureComponent {
+const mapDispatchToProps = (dispatch) => ({
+  onClearCompose:() => dispatch(clearCompose())
+})
+
+export default
+@connect(null, mapDispatchToProps)
+class Compose extends PureComponent {
+
+  static propTypes = {
+    onClearCompose: PropTypes.func.isRequired,
+  }
+
+  componentWillUnmount() {
+    this.props.onClearCompose()
+  }
 
   render () {
     return (
