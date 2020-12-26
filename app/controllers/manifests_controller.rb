3eeb0d5b950033bb79b2c030adc278dd8ab54508
mgabdev <>
2020-12-26 15:25:39 -0500

    Fixed issue with manifest.json route
    • Fixed:
    - issue with manifest.json route, now returning hardcoded json successfully

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
 class ManifestsController < EmptyController
-
   def show
-    render json: InstancePresenter.new, serializer: ManifestSerializer
+    render json: {
+      name: 'Gab Social',
+      short_name: 'Gab Social',
+      description: 'Gab is a social network that champions free speech, individual liberty and the free flow of information online. All are welcome.',
+      icons: [{
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      }],
+      theme_color: '#21D07B',
+      background_color: '#191b22',
+      display: 'standalone',
+      start_url: '/home',
+      scope: root_url,
+      share_target: {
+        action: '/compose',
+        method: 'GET',
+        params: {
+          title: 'title',
+          text: 'text',
+          url: 'url',
+        }
+      }
+    }
   end
-
 end
--- app/presenters/instance_presenter.rb
+++ app/presenters/instance_presenter.rb
@@ -9,26 +9,6 @@ class InstancePresenter
     to: Setting
   )
 
-  def contact_account
-    Account.find_local(Setting.site_contact_username.strip.gsub(/\A@/, ''))
-  end
-
-  def user_count
-    Rails.cache.fetch('user_count') { User.confirmed.joins(:account).merge(Account.without_suspended).count }
-  end
-
-  def active_user_count
-    Rails.cache.fetch('active_user_count') { Redis.current.pfcount(*(0..3).map { |i| "activity:logins:#{i.weeks.ago.utc.to_date.cweek}" }) }
-  end
-
-  def status_count
-    Rails.cache.fetch('local_status_count') { Account.local.joins(:account_stat).sum('account_stats.statuses_count') }.to_i
-  end
-
-  def domain_count
-    Rails.cache.fetch('distinct_domain_count') { Account.distinct.count(:domain) }
-  end
-
   def sample_accounts
     Rails.cache.fetch('sample_accounts', expires_in: 12.hours) { Account.discoverable.popular.limit(3) }
   end
