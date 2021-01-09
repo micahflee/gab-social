a32c5a453bf395f4536bc86a8682fefe7700ed71
Fosco Marotto <f@gab.com>
2021-01-09 15:44:20 -0500

    [merge_worker] disable

--- app/workers/merge_worker.rb
+++ app/workers/merge_worker.rb
@@ -6,6 +6,6 @@ class MergeWorker
   sidekiq_options queue: 'pull'
 
   def perform(from_account_id, into_account_id)
-    FeedManager.instance.merge_into_timeline(Account.find(from_account_id), Account.find(into_account_id))
+    #FeedManager.instance.merge_into_timeline(Account.find(from_account_id), Account.find(into_account_id))
   end
 end
