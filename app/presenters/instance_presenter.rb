a703b5cb78ab0da2525912448c7acf718921ac00
mgabdev <>
2021-01-15 15:04:44 -0500

    Removed unused values, functions in InstancePresenter
    • Removed:
    - unused values, functions in InstancePresenter

--- app/presenters/instance_presenter.rb
+++ app/presenters/instance_presenter.rb
@@ -2,16 +2,11 @@
 
 class InstancePresenter
   delegate(
-    :site_contact_email,
     :site_title,
     :site_short_description,
     :site_description,
     to: Setting
   )
-
-  def sample_accounts
-    Rails.cache.fetch('sample_accounts', expires_in: 12.hours) { Account.discoverable.popular.limit(3) }
-  end
 
   def version_number
     GabSocial::Version
@@ -19,10 +14,6 @@ class InstancePresenter
 
   def source_url
     GabSocial::Version.source_url
-  end
-
-  def thumbnail
-    @thumbnail ||= Rails.cache.fetch('site_uploads/thumbnail') { SiteUpload.find_by(var: 'thumbnail') }
   end
   
 end
