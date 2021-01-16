8b0f566e4faf09f62b14d9994e3c3ee294065bef
Fosco Marotto <f@gab.com>
2021-01-16 13:36:19 -0500

    [redis] Example change to redis pooling code.

--- app/controllers/admin/expenses_controller.rb
+++ app/controllers/admin/expenses_controller.rb
@@ -1,10 +1,14 @@
 class Admin::ExpensesController < Admin::BaseController
 	def index
-    @amount = Redis.current.get("monthly_funding_amount") || 0
+		Redis.current.with do |conn|
+			@amount = conn.get("monthly_funding_amount") || 0
+		end
 	end
 
   def create
-    Redis.current.set("monthly_funding_amount", params[:amount])
+		Redis.current.with do |conn|
+			conn.set("monthly_funding_amount", params[:amount])
+		end
 		redirect_to admin_expenses_path
 	end
 
